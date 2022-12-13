4189743b6f58b4f637b3e84e068651e94b5d525d
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-14 00:36:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { Route, Routes, NavLink } from 'react-router-dom';
 export const App = () => {
   return (
     <>
-      <NavLink to="/">Home</NavLink>
-      <Routes suppressNoMatchWarning={true}>
+      {/* <NavLink to="/">Home</NavLink> */}
+      <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </>
