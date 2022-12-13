9a39e896e7275f2d2a19587c330a2e30f65548fa
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-13 23:53:54 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
+import * as moviesApi from '../services/movies-api';
+import { useState, useEffect } from 'react';
 import Section from 'components/Section/Section';
 import PageHeading from 'components/PageHeading/PageHeading';
+import MoviesList from 'components/MoviesList/MoviesList';
 
-export default function () {
+export default function Home() {
+  const [trandingMovies, setTrandingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrandingMovies = () =>
+      moviesApi
+        .fetchTrandingMovies()
+        .then(({ data: { results } }) => setTrandingMovies(results))
+        .catch(error => console.log(error))
+        .finally();
+
+    fetchTrandingMovies();
+  }, []);
+
   return (
     <>
       <Section>
-        <PageHeading />
+        <PageHeading text="Day`s Top" />
+        <MoviesList movies={trandingMovies} />
       </Section>
     </>
   );
