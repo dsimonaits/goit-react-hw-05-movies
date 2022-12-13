fced8a917ac0088ea634b7becc98a2442242af35
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-13 23:53:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import Home from 'pages/Home';
+import { Route, Routes, NavLink } from 'react-router-dom';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <NavLink to="/">Home</NavLink>
+      <Routes suppressNoMatchWarning={true}>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+  // return <Home />;
 };
