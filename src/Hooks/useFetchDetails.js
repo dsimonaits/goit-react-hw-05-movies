56473d1896aba5cc0b958fc5847e3e77a2dd4bda
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-19 23:44:59 +0200

    Create useFetchDetails.js

---
+++ src/Hooks/useFetchDetails.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieDetails } from 'services/movies-api';
+
+const useFetchDetails = () => {
+  const [details, setDetails] = useState(null);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    fetchMovieDetails(movieId).then(setDetails);
+  }, [movieId]);
+
+  return details;
+};
+
+export default useFetchDetails;
