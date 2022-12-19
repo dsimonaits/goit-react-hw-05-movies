5dc05f23a28b7c5f10d156d15661ac9934261b19
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-19 23:47:49 +0200

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const MovieCard = ({ movies }) => {
-  const imagesUrl = 'https://image.tmdb.org/t/p/w500/';
+  // const imagesUrl = 'https://image.tmdb.org/t/p/w500/';
   return (
     <>
       {movies.map(
