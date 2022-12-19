71342b66759d3e5e568c199e79e13636e8b2eaae
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-19 23:44:57 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ import Section from 'components/Section/Section';
 import PageHeading from 'components/PageHeading/PageHeading';
 import MoviesList from 'components/MoviesList/MoviesList';
 
-export default function Home() {
+const Home = () => {
   const [trandingMovies, setTrandingMovies] = useState([]);
 
   useEffect(() => {
     const fetchTrandingMovies = () =>
       moviesApi
         .fetchTrandingMovies()
-        .then(({ data: { results } }) => setTrandingMovies(results))
+        .then(({ results }) => setTrandingMovies(results))
         .catch(error => console.log(error))
         .finally();
 
@@ -26,4 +26,6 @@ export default function Home() {
       </Section>
     </>
   );
-}
+};
+
+export default Home;
