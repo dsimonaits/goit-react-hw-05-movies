13100776ab37f1ffb05c2d1677488707d04dcf69
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-20 23:38:36 +0200

    Update movies-api.js

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -4,7 +4,7 @@ const API_KEY = '4c2c309e2c0263a8769257d39e3c269a';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
-export async function fetchTrandingMovies() {
+export async function fetchTrendingMovies() {
   try {
     const response = await axios('trending/all/day', {
       params: {
@@ -20,6 +20,19 @@ export async function fetchTrandingMovies() {
 export async function fetchMovieDetails(movieId) {
   try {
     const response = await axios(`movie/${movieId}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchMovieCast(movieId) {
+  try {
+    const response = await axios(`movie/${movieId}/credits`, {
       params: {
         api_key: API_KEY,
       },
