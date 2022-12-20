7124db6f478a6d2eedb974ff4dbd04b45a3f7adb
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-20 23:38:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Layout from './Layout/Layout';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
+import Cast from 'pages/Cast';
+import Reviews from 'pages/Reviews';
 
 export const App = () => {
   return (
@@ -11,7 +13,10 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </>
