73db2120dbabe03bede5be0ea386717d0060ebf3
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-20 23:38:45 +0200

    Create useFetchCast.js

---
+++ src/Hooks/useFetchCast.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieCast } from 'services/movies-api';
+
+const useFetchCast = () => {
+  const [cast, setCast] = useState(null);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    fetchMovieCast(movieId).then(setCast);
+  }, [movieId]);
+
+  return cast;
+};
+
+export default useFetchCast;
