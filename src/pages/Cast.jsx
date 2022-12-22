fdf6f4b85e19a836d0985c7914c22c5ed0448eda
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-23 00:15:17 +0200

    Update Cast.jsx

--- src/pages/Cast.jsx
+++ src/pages/Cast.jsx
@@ -2,22 +2,23 @@ import useFetchCast from 'Hooks/useFetchCast';
 
 const Cast = () => {
   const credits = useFetchCast();
-  console.log(credits);
 
   const imageUrl = 'https://image.tmdb.org/t/p/w500/';
+
+  const noImage =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png?20200912122019';
 
   return (
     credits && (
       <ul>
-        {credits.cast.map(({ profile_path, name, character }) => {
+        {credits.cast.map(({ profile_path, name, character, id }) => {
+          const actorPhoto = !profile_path ? noImage : imageUrl + profile_path;
           return (
-            <>
-              <li key={credits.id}>
-                <img src={imageUrl + profile_path} alt={name} width="200"></img>
-                <p>Name: {name}</p>
-                <p>Character: {character}</p>
-              </li>
-            </>
+            <li key={id}>
+              <img src={actorPhoto} alt={name} width="200"></img>
+              <p>Name: {name}</p>
+              <p>Character: {character}</p>
+            </li>
           );
         })}
       </ul>
