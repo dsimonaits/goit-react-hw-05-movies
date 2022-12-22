1a57f826317db37396ad459d7c7fadf4236cebe2
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-23 00:15:21 +0200

    Update useFetchDetails.js

--- src/Hooks/useFetchDetails.js
+++ src/Hooks/useFetchDetails.js
@@ -1,13 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieDetails } from 'services/movies-api';
 
 const useFetchDetails = () => {
+  const firstRender = useRef(true);
   const [details, setDetails] = useState(null);
-
   const { movieId } = useParams();
 
   useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
     fetchMovieDetails(movieId).then(setDetails);
   }, [movieId]);
 
