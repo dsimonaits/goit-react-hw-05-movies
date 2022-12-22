b6222eea056ea595268e1b5d7e4ad427a772645c
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-23 00:15:27 +0200

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,22 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const MovieCard = ({ movies }) => {
-  // const imagesUrl = 'https://image.tmdb.org/t/p/w500/';
+  const location = useLocation();
+  console.log(movies);
+
+  const imagesUrl = 'https://image.tmdb.org/t/p/w500/';
+
+  const noImage =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png?20200912122019';
   return (
-    <>
-      {movies.map(
-        ({ id, poster_path: image, original_title: title, overview }) => {
+    movies && (
+      <>
+        {movies.map(({ id, poster_path, name, title, overview }) => {
           const idToString = id.toString();
           return (
-            <Link key={id} to={`movies/${idToString}`}>
-              <h2>{title}</h2>
-              {/* <img src={imagesUrl + image} alt={title} /> */}
-              {/* <p>{overview}</p> */}
+            <Link
+              key={id}
+              to={
+                location.pathname === '/movies'
+                  ? `${idToString}`
+                  : `movies/${idToString}`
+              }
+            >
+              <h2>{name ?? title}</h2>
+              <img
+                width="200"
+                src={!poster_path ? noImage : imagesUrl + poster_path}
+                alt={name}
+              />
             </Link>
           );
-        }
-      )}
-    </>
+        })}
+      </>
+    )
   );
 };
 
