b9579751f6632fd120ed18823546953842b2ce43
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-23 00:15:23 +0200

    Update useFetchCast.js

--- src/Hooks/useFetchCast.js
+++ src/Hooks/useFetchCast.js
@@ -1,13 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieCast } from 'services/movies-api';
 
 const useFetchCast = () => {
+  const firstRender = useRef(true);
   const [cast, setCast] = useState(null);
-
   const { movieId } = useParams();
 
   useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
     fetchMovieCast(movieId).then(setCast);
   }, [movieId]);
 
