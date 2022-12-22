7f85ee512c9eab30ed77a91f68d93885d073e477
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-23 00:15:13 +0200

    Update Reviews.jsx

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,3 +1,24 @@
-const Reviews = () => {};
+import useFetchReviews from 'Hooks/useFetchReviews';
+
+const Reviews = () => {
+  const reviews = useFetchReviews();
+  return (
+    reviews &&
+    (reviews.results.length === 0 ? (
+      <p>There is no reviews yet!</p>
+    ) : (
+      <ul>
+        {reviews.results.map(({ author, content, id }) => {
+          return (
+            <li key={id}>
+              <h3>{author}</h3>
+              <p>{content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    ))
+  );
+};
 
 export default Reviews;
