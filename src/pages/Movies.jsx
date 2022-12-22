03c8513d2492ce9875705f7a5867fc3384ac614b
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-23 00:15:15 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,45 @@
-function Movies() {}
+// import { useState, useEffect } from 'react';
+import { useSearchParams, useLocation } from 'react-router-dom';
+import useFetchMoviesByName from 'Hooks/useFetchByQuery';
+import MoviesList from 'components/MoviesList/MoviesList';
+
+function Movies() {
+  /* eslint-disable */
+  const [_, setSearchParams] = useSearchParams();
+  /* eslint-enable */
+
+  const location = useLocation();
+  console.log(location);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const value = form.elements.query.value;
+    if (value.trim() === '') {
+      return;
+    }
+    setSearchParams({ moviename: value });
+
+    form.reset();
+  };
+
+  const data = useFetchMoviesByName();
+  console.log(data);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" />
+        <button>Search</button>
+      </form>
+      {data &&
+        (data.results.length === 0 ? (
+          <p>Nothing here matches your search !</p>
+        ) : (
+          <MoviesList movies={data.results} />
+        ))}
+    </>
+  );
+}
 
 export default Movies;
