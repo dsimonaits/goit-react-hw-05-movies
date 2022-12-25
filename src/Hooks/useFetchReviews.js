aa6867d8891c621713eb6dfa006ce5cde3d986f6
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-25 12:14:05 +0200

    Update useFetchReviews.js

--- src/Hooks/useFetchReviews.js
+++ src/Hooks/useFetchReviews.js
@@ -1,17 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieReviews } from 'services/movies-api';
 
 const useFetchReviews = () => {
-  const firstRender = useRef(true);
   const [reviews, setReviews] = useState(null);
   const { movieId } = useParams();
 
   useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
     fetchMovieReviews(movieId).then(setReviews);
   }, [movieId]);
 
