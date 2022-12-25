bd43a986e9997df72c3b4e16e05999365c9d090b
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-25 12:14:11 +0200

    Update useFetchByQuery.js

--- src/Hooks/useFetchByQuery.js
+++ src/Hooks/useFetchByQuery.js
@@ -1,20 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { fetchMovieByQuery } from 'services/movies-api';
 
 const useFetchMoviesByName = () => {
-  const firstRender = useRef(true);
   const [movies, setMovies] = useState(null);
 
   const [searchParams] = useSearchParams();
   const query = searchParams.get('moviename');
-
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
-  });
 
   useEffect(() => {
     if (query === null || query === '') return;
