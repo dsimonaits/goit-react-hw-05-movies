b322535bc93c270b23a7e174193fca7b4297d0f2
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-26 01:13:55 +0200

    Create ActorList.jsx

---
+++ src/components/ActorList/ActorList.jsx
@@ -0,0 +1,26 @@
+import { noImage } from 'images/noImage';
+import { ActorListStyled, ActorLIstItem } from './ActorList.styled';
+
+const ActorList = ({ data }) => {
+  const imageUrl = 'https://image.tmdb.org/t/p/w500/';
+  return (
+    <ActorListStyled>
+      <>
+        {data.cast.map(({ profile_path, name, character, id }) => {
+          const actorPhoto = !profile_path ? noImage : imageUrl + profile_path;
+          return (
+            <ActorLIstItem key={id}>
+              <img src={actorPhoto} alt={name} width="100"></img>
+              <div>
+                <p>{name}</p>
+                <p>Character: {character}</p>
+              </div>
+            </ActorLIstItem>
+          );
+        })}
+        ;
+      </>
+    </ActorListStyled>
+  );
+};
+export default ActorList;
