d6c8c5b6e44adb30012f9719fb9a916f58e4f65b
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-25 13:22:24 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,20 +1,28 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, Link, useLocation } from 'react-router-dom';
 import useFetchDetails from 'Hooks/useFetchDetails';
 import MovieDetailsCard from 'components/MovieDetailsCard/MovieDetailsCard';
 
 const MovieDetails = () => {
   const movieDetails = useFetchDetails();
+  const location = useLocation();
+  const mainLocationState = location.state.from;
+  const backLinkHref = location.state?.from ?? '/home';
   return (
     <>
+      <Link to={backLinkHref}>Back</Link>
       {movieDetails && <MovieDetailsCard movie={movieDetails} />}
       <div>
         <p>Additional information</p>
         <ul>
           <li>
-            <NavLink to="cast">Cast</NavLink>
+            <NavLink to="cast" state={{ from: mainLocationState }}>
+              Cast
+            </NavLink>
           </li>
           <li>
-            <NavLink to="reviews">Reviews</NavLink>
+            <NavLink to="reviews" state={{ from: mainLocationState }}>
+              Reviews
+            </NavLink>
           </li>
         </ul>
       </div>
