54f204ac31c27cf6cc502d9d09facf3db7b7b4d5
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-26 01:14:39 +0200

    Update MovieDetailsCard.jsx

--- src/components/MovieDetailsCard/MovieDetailsCard.jsx
+++ src/components/MovieDetailsCard/MovieDetailsCard.jsx
@@ -1,36 +1,49 @@
-import { useLocation } from 'react-router-dom';
+import { noImage } from 'images/noImage';
+import AdditionaLInfo from 'components/AdditionalInfo/AdditionalInfo';
+import { Outlet } from 'react-router-dom';
+import {
+  Wrapper,
+  MovieIMg,
+  MovieDetailsWrapper,
+  Title,
+  UserScore,
+  OverviewList,
+  OverviewItem,
+} from './MovieDetailsCard.styled';
 
-const MovieDetailsCard = ({ movie }) => {
-  const { poster_path, original_title, popularity, overview, genres } = movie;
+const MovieDetailsCard = ({ movie, mainState }) => {
+  const { poster_path, original_title, vote_average, overview, genres } = movie;
   const movieGenres = genres.map(genre => genre.name).join(', ');
-
-  const location = useLocation();
-  console.log(location);
 
   const imageUrl = 'https://image.tmdb.org/t/p/w500/';
   return (
     <>
-      <div>
-        <img
-          src={imageUrl + poster_path}
+      <Wrapper>
+        <MovieIMg
+          src={!poster_path ? noImage : imageUrl + poster_path}
           alt={original_title}
-          width="300"
-        ></img>
-        <div>
-          <h2>{original_title}</h2>
-          <ul>
-            <li>User Score: {popularity}</li>
-            <li>
+          width="300px"
+        ></MovieIMg>
+        <MovieDetailsWrapper>
+          <Title>{original_title}</Title>
+          <UserScore>
+            User Score: {Math.ceil(vote_average * 10) + '%'}
+          </UserScore>
+          <OverviewList>
+            <OverviewItem>
               <h3>Overview</h3>
               <p>{overview}</p>
-            </li>
-            <li>
+            </OverviewItem>
+            <OverviewItem>
               <h4>Genres</h4>
               <p>{movieGenres}</p>
-            </li>
-          </ul>
-        </div>
-      </div>
+            </OverviewItem>
+          </OverviewList>
+
+          <AdditionaLInfo mainState={mainState} />
+          <Outlet />
+        </MovieDetailsWrapper>
+      </Wrapper>
     </>
   );
 };
