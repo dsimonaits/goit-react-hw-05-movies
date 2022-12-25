b9bea9da45be2e14f41d9525250b2c9f3929672e
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-26 01:14:10 +0200

    Create ReviewsList.styled.js

---
+++ src/components/ReviewsLIst/ReviewsList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ReviewsItem = styled.li`
+  list-style: disc;
+  margin-left: 10px;
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
