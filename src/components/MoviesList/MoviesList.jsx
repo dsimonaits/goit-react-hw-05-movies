7358ed38cd2ad755fe70819fe7f8235b40384192
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-26 22:30:01 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,12 +1,19 @@
+import PropTypes from 'prop-types';
 import MovieCard from 'components/MovieCard/MovieCard';
 import { MoviesListStyled } from './MoviesList.styled';
+// import Btn from 'components/Btn/Btn';
 
 const MoviesList = ({ movies }) => {
   return (
     <MoviesListStyled>
       <MovieCard movies={movies} />
+      {/* <Btn text="Load more" /> */}
     </MoviesListStyled>
   );
 };
 
 export default MoviesList;
+
+MoviesList.propTypes = {
+  movies: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+};
