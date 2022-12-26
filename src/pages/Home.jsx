31a6019e3e6d6001c3692b72ee6e9e47bb21d573
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-26 22:30:13 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,74 @@
 import * as moviesApi from '../services/movies-api';
 import { useState, useEffect } from 'react';
 import Section from 'components/Section/Section';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import PageHeading from 'components/PageHeading/PageHeading';
 import MoviesList from 'components/MoviesList/MoviesList';
 import Container from 'components/Container/Container';
+import Btn from 'components/Btn/Btn';
+import { useRef } from 'react';
 
 const Home = () => {
+  const [status, setStatus] = useState('idle');
   const [trendingMovies, setTrendingMovies] = useState([]);
 
-  useEffect(() => {
-    const trendingMovies = () =>
-      moviesApi
-        .fetchTrendingMovies()
-        .then(({ results }) => setTrendingMovies(results))
-        .catch(error => console.log(error))
-        .finally();
+  const [page, setPage] = useState(1);
 
-    trendingMovies();
-  }, []);
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    setStatus('idle');
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    const trendingMoviesFetch = () => Loading.circle();
+    moviesApi
+      .fetchTrendingMovies(page)
+      .then(({ results }) => {
+        if (results.length === 0) {
+          setStatus('rejected');
+        } else {
+          setStatus('pending');
+        }
+
+        setTrendingMovies(prevState => {
+          const filteredResult = [];
+          const stateMoviesId = prevState.map(obj => obj.id);
+          results.map(result => {
+            if (!stateMoviesId.includes(result.id)) {
+              filteredResult.push(result);
+            }
+            setStatus('approved');
+            return filteredResult;
+          });
+
+          return [...prevState, ...filteredResult];
+        });
+      })
+      .catch(error => console.log(error))
+      .finally();
+
+    trendingMoviesFetch();
+    Loading.remove();
+  }, [page]);
+
+  const loadMore = () => {
+    setPage(page + 1);
+  };
 
   return (
     <Section>
       <Container>
         <PageHeading text="Day`s Top" />
-        <MoviesList movies={trendingMovies} />
+        {trendingMovies && (
+          <>
+            <MoviesList movies={trendingMovies} />
+            {status === 'approved' && (
+              <Btn text="Load more" onclickHandler={loadMore} />
+            )}
+          </>
+        )}
       </Container>
     </Section>
   );
